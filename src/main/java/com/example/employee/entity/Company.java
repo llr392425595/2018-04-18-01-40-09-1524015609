a8d26cc18564c158d7ff05d9c6f5d6b0ac26ed5c
llr392425595 <392425595@qq.com>
2018-04-18 14:01:09 +0800

    完成Employee类与Company类实体

--- src/main/java/com/example/employee/entity/Company.java
+++ src/main/java/com/example/employee/entity/Company.java
@@ -1,5 +1,52 @@
 package com.example.employee.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Company {
 
+  @Id
+  @GeneratedValue
+  private Integer id;
+  private String companyName;
+  private Integer employeesNumber;
+
+  public Company() { }
+
+  public Company(Integer id, String companyName, Integer employeesNumber) {
+    this.id = id;
+    this.companyName = companyName;
+    this.employeesNumber = employeesNumber;
+  }
+
+  public Company(String companyName, Integer employeesNumber) {
+    this.companyName = companyName;
+    this.employeesNumber = employeesNumber;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getCompanyName() {
+    return companyName;
+  }
+
+  public void setCompanyName(String companyName) {
+    this.companyName = companyName;
+  }
+
+  public Integer getEmployeesNumber() {
+    return employeesNumber;
+  }
+
+  public void setEmployeesNumber(Integer employeesNumber) {
+    this.employeesNumber = employeesNumber;
+  }
 }
--- src/main/java/com/example/employee/entity/Employee.java
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -1,5 +1,81 @@
 package com.example.employee.entity;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Employee {
 
+  @Id
+  @GeneratedValue
+  private Integer id;
+
+  private String name;
+  private Integer age;
+  private String gender;
+  private Integer salary;
+  private Integer companyId;
+
+  public Employee() {
+  }
+
+  public Employee(String name, Integer age, String gender, Integer salary,
+      Integer id, Integer companyId) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.salary = salary;
+    this.companyId = companyId;
+    this.id = id;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+
+  public Integer getSalary() {
+    return salary;
+  }
+
+  public void setSalary(Integer salary) {
+    this.salary = salary;
+  }
+
+  public Integer getCompanyId() {
+    return companyId;
+  }
+
+  public void setCompanyId(Integer companyId) {
+    this.companyId = companyId;
+  }
 }
